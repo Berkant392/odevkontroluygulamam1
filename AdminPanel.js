94ac0124d3e4f32fb2c77746d4ca90b4fc03f0b4
Berkant392 <mertrecberogullari@gmail.com>
2026-05-11 18:29:39 +0300

    Create AdminPanel.js

---
+++ AdminPanel.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const AdminPanel = ({ classes, onAddClass, onDeleteClass }) => {
+    return (
+        <div className="flex flex-col gap-8">
+            <div className="flex justify-between items-center">
+                <h2 className="text-xl font-bold">Sınıf Yönetimi</h2>
+                {/* Sınıf Ekleme Butonları */}
+            </div>
+            {/* Tablolar ve Öğrenci Listeleri */}
+        </div>
+    );
+};
