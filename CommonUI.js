a50007a864ea6c2597af2fe762a3133e0f638a1c
Berkant392 <mertrecberogullari@gmail.com>
2026-05-11 18:30:12 +0300

    Create CommonUI.js

---
+++ CommonUI.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Countdown = () => {
+    // Sınav tarihi hesaplama mantığı burada
+    return (
+        <div className="max-w-6xl mx-auto px-4 mt-6">
+            {/* Sayaç Görseli */}
+        </div>
+    );
+};
+
+export const Header = ({ role, onLogout }) => { /* ... */ };
+export const Announcement = ({ isTeacher }) => { /* ... */ };
