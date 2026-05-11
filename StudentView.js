81790fa74e46285ab0d86bbd9361d49ea2247415
Berkant392 <mertrecberogullari@gmail.com>
2026-05-11 18:29:51 +0300

    Create StudentView.js

---
+++ StudentView.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const StudentView = ({ studentData, selectedClass }) => {
+    return (
+        <div className="bg-white rounded-3xl p-8 shadow-2xl">
+            {/* Öğrenci Başarı Kartları */}
+            {/* Ödev Listesi ve Durum Rozetleri */}
+        </div>
+    );
+};
